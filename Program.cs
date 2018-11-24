70a4dba89b52d305fbd26583ffe7bf087ff34974
seb16120 <45233573+seb16120@users.noreply.github.com>
2018-11-24 11:19:36 +0100

    I worked on Problem still 1 help

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+
 
 namespace C_sharp_learning_console
 {
@@ -45,14 +47,31 @@ namespace C_sharp_learning_console
         }
 
 
-        // Converts N/N/N integer format to 0-1 format
+        // Converts N/N/N integer format to 0-1 format (where RGB color integers are packed red/green/blue, in that order from lowest to highest bits):
         static double[] FromNNN(double rgb, double scale)
         {
+
+
+            // PROBLEM NUMBER 1
+            // The Math class in .Net does not contain a structure/proerty or function called "Rem", I'm not sure
+            // what it is your trying to do here, but "Rem" is not any kind of function I've heard of.  If it's to
+            // do with finding the remainder of a formula, then I believe it's the % operator you want.
+            // this may help : https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/remainder-operator …
+
+
+
+
+
+
+
             double sm1 = scale - 1;
-            double r = Math.Rem(rgb, scale); //what is rem ? i test Math.Rem but not work   <---- help
-            double g = rem(Math.Floor(rgb / scale), scale);
-            double b = rem(Math.Floor(rgb / (scale * scale)), scale);
+            double r = rgb % scale;
+            double g = Math.Floor(rgb / scale) % scale;
+            double b = Math.Floor(rgb / (scale * scale) % scale);
             return new double[] { r / sm1, g / sm1, b / sm1 };
+
+
+            
         }
 
 
@@ -75,22 +94,85 @@ namespace C_sharp_learning_console
         // It also takes the number of characters as a parameter named size. 
         // (How to convert this list to a text string depends on the programming language and is outside the scope of this page.)
         //
-        static List<Char> RandomString(List<char> characterList, int stringSize)
+        static ArrayList RandomString(int stringSize = 6)
         {
 
 
             int i = 0;
-            List<Char> newString = new List<Char>();
+            
+            ArrayList characterList = new ArrayList();
+            characterList.Add("A");
+            characterList.Add("B");
+            characterList.Add("C");
+            characterList.Add("D");
+            characterList.Add("E");
+            characterList.Add("F");
+            characterList.Add("G");
+            characterList.Add("H");
+            characterList.Add("I");
+            characterList.Add("J");
+            characterList.Add("K");
+            characterList.Add("L");
+            characterList.Add("M");
+            characterList.Add("N");
+            characterList.Add("O");
+            characterList.Add("P");
+            characterList.Add("Q");
+            characterList.Add("R");
+            characterList.Add("S");
+            characterList.Add("T");
+            characterList.Add("U");
+            characterList.Add("V");
+            characterList.Add("W");
+            characterList.Add("X");
+            characterList.Add("Y");
+            characterList.Add("Z");
+            characterList.Add("0");
+            characterList.Add("1");
+            characterList.Add("2");
+            characterList.Add("3");
+            characterList.Add("4");
+            characterList.Add("5");
+            characterList.Add("6");
+            characterList.Add("7");
+            characterList.Add("8");
+            characterList.Add("9");
+
+            ArrayList word = new ArrayList();
+
+
+            Random random1 = new Random();
+            int randomNumber1 = random1.Next(0, characterList.Count);
+
+
+
             while (i < stringSize)
             {
+
+
+                // PROBLEM 2
+                // There is no Random character function in C#/.NET, you'll need to create your own using the Random class
+                // documentation on using the .NET random number generator can be found here :
+                // https://docs.microsoft.com/en-us/dotnet/api/system.random?view=netframework-4.7.2 …
+                //
+                // You'll need to generate a random number from 0 to 255, then use the string functions to turn that into a char
+
+
+
+
+
+
+
+
+
                 // Choose a character from the list
-                char randomChar = characterList[RNDINTEXC (characterList.Count)]; //need the name of the method of .method for RNDINTEXC    <---- help
+                word.Add(characterList[randomNumber1]); //Hope it work now
                 // Add the character to the string
-                newString.Add(randomChar); //
+                
                 i = i + 1;
             }
 
-           return newString;
+           return word;
         }
 
 
@@ -155,6 +237,15 @@ namespace C_sharp_learning_console
 
 
 
+        // PROBLEM 3
+        // YES, double[] color DOES declare the variable as an Array (Specifically an array of double values [Doubles are 64 bit numbers])
+        // your SRGBToLinear function, only takes a single double as it's parameter, and returns a single double as it's result
+        // this is why the 'color' has a red squiggle under it, beacuse your trying to pass the wrong type to your function
+
+
+
+
+
 
 
 
@@ -162,10 +253,10 @@ namespace C_sharp_learning_console
 
 
         // Convert encoded sRGB to luminance factor
-        static double LuminanceSRGB(double[] color) //not sure if color is a array  <---- help
+        static double LuminanceSRGB(double color) 
         {
             // Convert to linear sRGB
-            double[] c = SRGBToLinear(color);
+            double[] c = SRGBToLinear(color); // from this i need to deal it to c[0] / c[1] / c[3] for the R / G / B     <----  Help
             // Find the linear sRGB luminance factor
             return c[0] * 0.2126 + c[1] * 0.7152 + c[2] * 0.0722;
         }
@@ -175,7 +266,7 @@ namespace C_sharp_learning_console
         static double LuminanceSRGBD50(double color)
         {
             // Convert to linear sRGB
-            double[] c = SRGBToLinear(color);
+            double[] c = SRGBToLinear(color); //// from this i need to deal it to c[0] / c[1] / c[3] for the R / G / B
             // Find the linear sRGB luminance factor
             return c[0] * 0.2225 + c[1] * 0.7169 + c[2] * 0.0606;
         }
@@ -202,6 +293,15 @@ namespace C_sharp_learning_console
             }
 
 
+            // PROBLEM 5
+            // Your trying to pass in the wrong variable types as parameters to the function calls, in the case of 
+            // all 3, you have also not declared any variables of those names, so the compiler cannot find them
+
+            double c = 16777216;
+            double[] pixel = { 0, 1, 2 };
+            double color = 16777216;
+
+
             SRGBToLinear(c); //what i put in ?
             SRGBToLinear3(pixel); // i think i put _imageBuffer in.
             LuminanceSRGB(color); //i put in the RGB who was transform into sRGB linear
@@ -217,8 +317,17 @@ namespace C_sharp_learning_console
             Random random2 = new Random();
             int randomNumber2 = random2.Next(1); // i Hope its this for generate Random number beetween 0 and 1
 
+
+
+            // PROBLEM 6
+            // You CANNOT declare arrays in this manner in C#, it's not like JavaScript or PHP where you just build the array on the fly
+            // if you want to declare the contents of an array, you have to do it using the "new" constructor as follows
+            //  double[] randomColor = new double[]{ randomNumber2, randomNumber2, randomNumber2 }
+
+
+
             //Generating a random color in the 0-1 format is equivalent to generating this.
-            double[] randomColor = [randomNumber2, randomNumber2, randomNumber2]; 
+            double[] randomColor =  { randomNumber2, randomNumber2, randomNumber2 }; // don't need new double[] apparently
 
 
             unsafe
@@ -239,3 +348,14 @@ namespace C_sharp_learning_console
         }
     }
 }
+
+
+// Further Notes.....................
+// Looking at your code, I strikes me that you may be more used to either plain old C/C++ or JavaScript as a lot of the
+// mistakes your making are language syntax based mistakes, and are common patterns in C++ and JavaScript.
+//
+// May I suggest downloading something like : https://www.introprogramming.info/english-intro-csharp-book/downloads/ …
+// as this will take you through the differences between what your used to and wahts different.
+//
+// Remember, C# is a VERY, VERY strict language when it comes to enforcing data types, it's not as forgiving as other languages
+// and it will break if you try to do variable assignments it does not like :-)
